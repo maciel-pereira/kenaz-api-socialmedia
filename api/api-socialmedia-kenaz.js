d5ef28864a15a67ee9a13995c6dbcad591c9b6ee
Maciel Eustaquio <maciel@kenaz.com.br>
2020-05-09 12:12:41 -0300

    Cria novo teste de api.

--- api/api-socialmedia-kenaz.js
+++ api/api-socialmedia-kenaz.js
@@ -1,13 +1,13 @@
 var unirest = require("unirest");
 
-var req = unirest("GET", "https://twinesocial.p.rapidapi.com/v1/content");
+var req = unirest("GET", "https://easy-instagram-service.p.rapidapi.com/get-post");
 
 req.query({
-	"campaign": "kenazmarketing"
+	"shortcode": "B2yA71EgX6a"
 });
 
 req.headers({
-	"x-rapidapi-host": "twinesocial.p.rapidapi.com",
+	"x-rapidapi-host": "easy-instagram-service.p.rapidapi.com",
 	"x-rapidapi-key": "27fe776426msh3c45f78dbc25942p1af9b0jsn1bb56775318f"
 });
 
@@ -16,4 +16,4 @@ req.end(function (res) {
 	if (res.error) throw new Error(res.error);
 
 	console.log(res.body);
-});
+});status
